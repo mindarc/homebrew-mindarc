2ca558ebbd1ec606e861a43daf08441451567cd0
teekm@mbp2023 <tkimm@me.com>
2024-05-06 18:00:57 +0200

    ci: Update repo rm migration file

--- Formula/mindarc.rb
+++ Formula/mindarc.rb
@@ -1,12 +1,13 @@
 require "formula"
 require_relative "../custom_download_strategy.rb"
 
+
 class Mindarc < Formula
   homepage "https://www.mindarc.com.au"
-  url "https://github.com/mindarc/mindarc-devops/releases/download/v1.0.0/mindarc-cli-v1.0.0.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "210a619c56a2c523b8c80b88c601926a4443d7d633aab1be3766cc61fbc38dec"
+  url "https://github.com/mindarc/mindarc-cli/releases/download/v1.0.1/mindarc-v1.0.1-darwin-arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "8d231a552eb6b7e405b539a48157dc3c539a1e6112f34f4cc0c7770f5919f668"
   head "https://github.com/mindarc/mindarc-cli.git"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "mindarc"
